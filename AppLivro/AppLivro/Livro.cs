f6a5ad509197563e6b73d9d5de0eb9d42aef77d3
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-10-06 11:05:36 -0300

    AppLivro

---
+++ AppLivro/AppLivro/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLivro
+{
+    internal class Livro
+    {
+        protected string titulo;
+        protected string autor;
+        protected int anoPublicacao;
+
+        public Livro(string titulo, string autor, int anoPublicacao)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anoPublicacao = anoPublicacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"{titulo} - {autor}");
+        }
+    }
+}
---
+++ AppLivro/AppLivro/Program.cs
@@ -0,0 +1,15 @@
+﻿using AppLivro;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1984 = new Livro("1984", "George Orwell", 1949);
+
+            livro1984.ExibirInformacoes();
+        }
+    }
+}
